a74d94187b882c8f05d129b1bd321054335400c7
Hamilton Tumenas Borges <hamiltontubo@gmail.com>
2025-11-26 09:24:55 -0300

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,28 +4,34 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+describe("GET /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
+      const responseBody = await response.json();
 
-  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
-  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+      const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+      expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
-  expect(responseBody.dependencies.database.version).toEqual("16.0");
-  expect(responseBody.dependencies.database.version).not.toBeNull();
+      expect(responseBody.dependencies.database.version).toEqual("16.0");
+      expect(responseBody.dependencies.database.version).not.toBeNull();
 
-  expect(typeof responseBody.dependencies.database.max_connections).toBe(
-    "number",
-  );
-  expect(responseBody.dependencies.database.max_connections).toBeGreaterThan(0);
+      expect(typeof responseBody.dependencies.database.max_connections).toBe(
+        "number",
+      );
+      expect(
+        responseBody.dependencies.database.max_connections,
+      ).toBeGreaterThan(0);
 
-  expect(typeof responseBody.dependencies.database.opened_connections).toBe(
-    "number",
-  );
-  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
-  expect(
-    responseBody.dependencies.database.opened_connections,
-  ).toBeGreaterThanOrEqual(1);
+      expect(typeof responseBody.dependencies.database.opened_connections).toBe(
+        "number",
+      );
+      expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+      expect(
+        responseBody.dependencies.database.opened_connections,
+      ).toBeGreaterThanOrEqual(1);
+    });
+  });
 });
